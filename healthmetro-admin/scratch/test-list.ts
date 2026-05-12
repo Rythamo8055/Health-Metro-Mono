bfe82f9511802e09b2996cfa3879c39cb43571fb
Rythamo8055 <vishnuvardhanthe8055@gmail.com>
2026-05-12 20:48:43 +0530

    chore: cleanup scratch test files

--- healthmetro-admin/scratch/test-list.ts
+++
@@ -1,9 +0,0 @@
-import { createAdminClient } from '../utils/supabase/admin';
-
-async function test() {
-  const supabase = createAdminClient();
-  const { data, error } = await supabase.from('providers').select('id, status, client_id');
-  console.log(data);
-}
-
-test();
